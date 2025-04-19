777b5b6627f36ad068298b2678b58a3f5235a065
ammar3040 <ammar15102004@gmail.com>
2025-04-19 08:45:15 +0530

    Move CSS to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "../../nutrition mate react/assets/css/style.css"
+import "../public/assets/css/style.css"
 
 createRoot(document.getElementById('root')).render(
  
